436b44abcd599f4ae16787af6a97aaf44f9ba6f8
mfnsvrtm <74641121+mfnsvrtm@users.noreply.github.com>
2022-11-24 20:37:52 +0400

    refactor UI code
    - move canvas related code to the new GameRenderer class
    - adjust package structure (add the "ui" package)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.github.mfnsvrtm.SnakeGame {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.github.mfnsvrtm.snakegame.controller to javafx.fxml;
+    opens com.github.mfnsvrtm.snakegame.ui.controller to javafx.fxml;
     exports com.github.mfnsvrtm.snakegame;
 }
